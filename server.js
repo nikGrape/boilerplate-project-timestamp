eb6cf5c6494645c8bc3c0001c79a7b314a694a9c
Nikolai Vinogradov <nik@Nikolais-MacBook-Pro.local>
2022-04-27 22:08:09 +0300

    port update

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get("/api/:date?", (req, res) => {
 
 
 // listen for requests :)
-var listener = app.listen(3000 | process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
